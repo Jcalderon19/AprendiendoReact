44ed140a7c296f2bbac71a87cf131757ebe09f27
Juan David <jdcd426@gmail.com>
2025-02-10 12:12:55 -0500

    Primeras configuraciones

---
+++ projects/ReactFirts/src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+  <StrictMode>
+    
+      <button>Button</button>
+      <button>Button</button>
+      <button>Button</button>
+    
+  </StrictMode>
+);
+
+
